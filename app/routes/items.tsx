919f681349d9adb225bfccf7f7076ac8843c2b07
Ahmet Kaan Toprakçıoğlu <56438853+ahmettoprakcioglu@users.noreply.github.com>
2025-04-27 12:19:09 +0300

    Refactor app structure to integrate Supabase for item management, including loading and creating items. Update styles and remove unused CSS for improved clarity.

--- app/routes/items.tsx
+++ app/routes/items.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { supabase } from "~/supabase-client"
+import type { Route } from "./+types/items";
+import { Link } from "react-router";
 
-const Items = () => {
+export function meta() {
+  return [
+    { title: `All Your Items | RRV7 Crud` },
+    {
+      name: "description",
+      content:
+        "Manage your items - view, create, and update items using our Supabase CRUD app.",
+    },
+  ];
+}
+
+export const loader = async () => {
+  const { data, error } = await supabase.from('items').select('*');
+
+  if (error) {
+    return {
+      error: error.message
+    }
+  }
+
+  return { items: data }
+}
+
+const Items = ({ loaderData }: Route.ComponentProps) => {
+  const { error, items = [] } = loaderData;
+
   return (
     <div>
-      Items Page
+      <h2 className="text-2xl font-bold text-gray-800 mb-4"> List of Items</h2>
+      {error && (
+        <div className="bg-red-200 text-red-800 p-2 mb-4 rounded">
+          {" "}
+          {error}{" "}
+        </div>
+      )}
+      <ul className="space-y-4">
+        {items?.map((item, index) => (
+          <li className="p-4 bg-white rounded shadow" key={index}>
+            <Link to={`/items/${item.id}`} className="block text-indigo-600">
+              <span className="font-semibold text-xl">{item.title}</span>
+              <p className="text-gray-700">{item.description}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
--- app/routes/newItem.tsx
+++ app/routes/newItem.tsx
@@ -1,7 +1,75 @@
+import { Form, redirect, type ActionFunctionArgs, useNavigation } from "react-router"
+import { supabase } from "~/supabase-client";
+
+export const meta = () => {
+  return [
+    {
+      title: 'New Item | RRV7 Crud'
+    }, {
+      name: 'description',
+      content: 'Create a new item using our Supabase CRUD app.'
+    }
+  ]
+}
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const formData = await request.formData();
+  const title = await formData.get('title') as string;
+  const description = await formData.get('description') as string;
+
+  if (!title || !description) {
+    return {
+      error: 'Title and description are required'
+    }
+  }
+
+  const { error } = await supabase.from('items').insert({ title, description });
+
+  if (error) {
+    return { error: error?.message }
+  }
+
+  return redirect('/')
+}
+
 const NewItem = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
   return (
-    <div>
-      NewItem Page
+    <div className="max-w-md mx-auto">
+      <h2 className="text-2xl font-bold mb-4">Create New Item</h2>
+      <Form method="post" className="space-y-4 bg-white p-4 rounded shadow">
+        <div>
+          <label className="block text-gray-700">Title</label>
+          <input
+            name="title"
+            type="text"
+            className="border border-gray-300 rounded px-3 py-2 w-full"
+            required
+          />
+        </div>
+        <div>
+          <label className="block text-gray-700">Content</label>
+          <textarea
+            name="description"
+            className="border border-gray-300 rounded px-3 py-2 w-full"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          disabled={isSubmitting}
+          className={`${
+            isSubmitting ? 'bg-green-400' : 'bg-green-600 hover:bg-green-500'
+          } text-white px-4 py-2 rounded cursor-pointer disabled:cursor-not-allowed flex items-center justify-center gap-2`}
+        >
+          {isSubmitting && (
+            <div className="h-4 w-4 animate-spin rounded-full border-2 border-white border-r-transparent" />
+          )}
+          {isSubmitting ? 'Creating...' : 'Create Item'}
+        </button>
+      </Form>
     </div>
   )
 }
